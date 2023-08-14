b7c4103f7e3d93c3a60afb2a03a0cf23dc2af5c7
Gabriel Flores <108844543+GabrielFlores8227@users.noreply.github.com>
2023-08-14 18:31:30 -0300

    done: translate script

--- scripts/builder.js
+++ scripts/builder.js
@@ -3,7 +3,15 @@ import * as modules from './modules.js';
 (async () => {
 	let req = await (await fetch('/builder/builder.json')).json();
 
-	req = req.en;
+	const userLanguage = navigator.language;
+
+	if (req[userLanguage]) {
+		window.document.documentElement.lang = userLanguage;
+
+		req = req[userLanguage];
+	} else {
+		req = req.en;
+	}
 
 	modules.buildHeader(req.header);
 })();
