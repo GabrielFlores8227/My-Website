90b7a44f83537a389d8223d09c464caa70a0c7a9
Gabriel Flores <108844543+GabrielFlores8227@users.noreply.github.com>
2023-08-14 17:58:44 -0300

    done: header section finished

--- scripts/modules.js
+++ scripts/modules.js
@@ -1,16 +1,52 @@
 export function buildHeader(header) {
-	const sectionsTemplate = window.document.querySelector(
-		'template[sections-template]',
+	window.document
+		.querySelectorAll('template[sections-template]')
+		.forEach((template) => {
+			header.sections.forEach((section, index) => {
+				const usableTemplate = template.content.cloneNode(true).children[0];
+
+				if (section.special) {
+					usableTemplate.classList.add('--special');
+
+					const a = usableTemplate.querySelector('a');
+
+					a.innerText = `${section.name}`;
+					a.setAttribute('href', section.to);
+					a.setAttribute('target', '_blank');
+
+					template.parentNode.append(usableTemplate);
+
+					return;
+				}
+
+				usableTemplate.classList.add('--default');
+
+				const a = usableTemplate.querySelector('a');
+
+				a.innerText = `${index + 1}. ${section.name}`;
+				a.setAttribute('href', section.to);
+
+				template.parentNode.append(usableTemplate);
+			});
+		});
+
+	const drawerButton = window.document.querySelector(
+		'button[header-drawer--on]',
 	);
 
-	header.sections.forEach((section, index) => {
-		const usableTemplate = sectionsTemplate.content.cloneNode(true).children[0];
+	const drawer = window.document.querySelector('div[header-drawer]');
 
-		const a = usableTemplate.querySelector('a');
+	drawerButton.addEventListener('click', () => {
+		const state = drawer.classList.contains('--on');
 
-		a.innerText = `${index + 1}. ${section.name}`;
-		a.setAttribute('href', section.to);
+		if (state) {
+			drawer.classList.remove('--on');
+		} else {
+			drawer.classList.add('--on');
+		}
+	});
 
-		sectionsTemplate.parentNode.append(usableTemplate);
+	drawerButton.addEventListener('focusout', () => {
+		drawer.classList.remove('--on');
 	});
 }
